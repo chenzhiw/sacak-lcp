c199b496431d6fc3599f552d145dc50ceaeca682
Felipe A. Louza <felipelouza@gmail.com>
2018-01-17 09:56:34 -0200

    Fixed small bug (getline)

--- experiments/lib/file.c
+++ experiments/lib/file.c
@@ -1,5 +1,7 @@
 #include "file.h"
 
+/* Returns the file extension
+ */
 const char *get_filename_ext(const char *filename) {                                                  
     const char *dot = strrchr(filename, '.');                                                         
     if(!dot || dot == filename) return "";                                                            
@@ -9,7 +11,7 @@ const char *get_filename_ext(const char *filename) {
 /*******************************************************************/
 
 /* Changes to a working directory, where everything will be read
- * from and written to:
+ * from and written to
  */ 
 int file_chdir(char* dir){
 	
@@ -112,11 +114,11 @@ char** load_multiple_txt(FILE* f_in, int k, int_t *n) {
 
 	int i;
  	for(i=0; i<k; i++){
-		size_t len = 0;
-		c_buffer[i] = NULL;
-			
+
+		size_t len = 0; c_buffer[i] = NULL;		
 		ssize_t size = getline(&c_buffer[i], &len, f_in);
 		if (size == -1){
+			printf("K = %d\n", i);
 			return 0;
 		}
 		c_buffer[i][size-1] = 0;
@@ -133,22 +135,23 @@ char** load_multiple_fastq(FILE* f_in, int k, int_t *n){
 
 	char **c_buffer = (char**) malloc(k*sizeof(char*));
 
+  size_t len = 0;
+	char *buf = NULL;
 	int i;
  	for(i=0; i<k; i++){
 
-                size_t len = 0;
-		char *buf = NULL;
-
+		len = 0; buf = NULL;
 		ssize_t size = getline(&buf, &len, f_in); // @'s line
-                if (size <= 1)
-                        return 0;
+    if (size <= 1)
+    return 0;
 
-                c_buffer[i] = NULL;
+		len = 0; c_buffer[i] = NULL;
 		size = getline(&c_buffer[i], &len, f_in); // read line
-                c_buffer[i][size-1] = 0;
+    c_buffer[i][size-1] = 0;
 
-                (*n) += size;
+    (*n) += size;
 
+		len = 0; buf = NULL;
 		getline(&buf, &len, f_in); // +'s line
 		getline(&buf, &len, f_in); // @'s line
 
@@ -175,11 +178,11 @@ char** load_multiple_fasta(FILE* f_in, int k, int_t *n){
 
 		if(i!=count) return 0;
 
-		len = 0;
 		int nalloc = 128;
 		c_buffer[i] = malloc(nalloc*sizeof(char));
 
 		int_t p=0;
+		len = 0; buf=NULL;
 		while(getline(&buf, &len, f_in)!=-1){
 
 			if(buf[0] == '>'){
@@ -188,7 +191,7 @@ char** load_multiple_fasta(FILE* f_in, int k, int_t *n){
 			}
 
 			if(p+len>nalloc){
-				nalloc += 128;
+				nalloc += len+128;
 				c_buffer[i] = realloc(c_buffer[i], sizeof(char) * nalloc);
 			}
 
@@ -229,10 +232,143 @@ char** file_load_multiple(char* c_file, int k, int_t *n) {
 
 	else if(strcmp(type,"fastq") == 0)
 		c_buffer = load_multiple_fastq(f_in, k, n);
+	else{
+		printf("Error: file not recognized (.txt, .fasta, .fastq)\n");
+		return 0;
+	}
 
 	fclose(f_in);
 
 return c_buffer;
 }
+
+/*******************************************************************/
+
+int file_text_write(unsigned char *str, int_t n, char* c_file, const char* ext){
+
+	FILE *f_out;
+	char *c_out = (char*) malloc((strlen(c_file)+strlen(ext)+3)*sizeof(char));
+	
+	sprintf(c_out, "%s.%s", c_file, ext);
+	f_out = file_open(c_out, "wb");
+	
+	int i;
+	for(i=0; i<n;i++) if(str[i]) str[i]--;
+
+	fwrite(str, sizeof(unsigned char), n, f_out);
+	
+	file_close(f_out);
+	free(c_out);
+
+return 1;
+}
+
+int file_text_int_write(int_t *str, int_t n, char* c_file, const char* ext){
+
+	FILE *f_out;
+	char *c_out = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_out, "%s.%s", c_file, ext);
+	f_out = file_open(c_out, "wb");
+	
+	fwrite(str, sizeof(int_t), n, f_out);
+	
+	file_close(f_out);
+	free(c_out);
+
+return 1;
+}
+
+/*******************************************************************/
+
+int_t file_text_read(unsigned char** str, char* c_file, const char* ext){
+
+	FILE *f_in;
+	char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_in, "%s.%s", c_file, ext);
+	f_in = file_open(c_in, "rb");
+
+	fseek(f_in, 0L, SEEK_END);
+	size_t size = ftell(f_in);
+	rewind(f_in);
+
+	int_t n = size/sizeof(unsigned char);
+
+	*str = (unsigned char*) malloc(n*sizeof(unsigned char));
+	fread(*str, sizeof(unsigned char), n, f_in);
+	
+	file_close(f_in);
+	free(c_in);
+
+return n;
+}
+
+int_t file_text_int_read(int_t** str_int, char* c_file, const char* ext){
+
+	FILE *f_in;
+	char *c_in = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_in, "%s.%s", c_file, ext);
+	f_in = file_open(c_in, "rb");
+
+	fseek(f_in, 0L, SEEK_END);
+	size_t size = ftell(f_in);
+	rewind(f_in);
+
+	int_t n = size/sizeof(int_t);
+
+	*str_int = (int_t*) malloc(n*sizeof(int_t));
+	fread(*str_int, sizeof(int_t), n, f_in);
+	
+	file_close(f_in);
+	free(c_in);
+
+return n;
+}
+
+/*******************************************************************/
+
+int file_bwt_write(unsigned char *str, int_t *SA, int_t n, char* c_file, const char* ext){
+
+	FILE *f_out;
+	char *c_out = (char*) malloc((strlen(c_file)+strlen(ext)+3)*sizeof(char));
+	
+	sprintf(c_out, "%s.%s", c_file, ext);
+	f_out = file_open(c_out, "wb");
+	
+	int i;
+	for(i=0; i<n;i++){
+		char j = (SA[i])? str[SA[i]-1]:'#';
+		if(j==0) j = '$';
+		fwrite(&j, sizeof(unsigned char), 1, f_out);
+	}
+	
+	file_close(f_out);
+	free(c_out);
+
+return 1;
+}
+
+int file_bwt_int_write(int_t *str, int_t *SA, int_t n, char* c_file, const char* ext){
+
+	FILE *f_out;
+	char *c_out = malloc((strlen(c_file)+strlen(ext))*sizeof(char));
+	
+	sprintf(c_out, "%s.%s", c_file, ext);
+	f_out = file_open(c_out, "wb");
+	
+	int i;
+	for(i=0; i<n;i++){
+		int_t j = (SA[i])? str[SA[i]-1]:0;
+		fwrite(&j, sizeof(int_t), 1, f_out);
+	}
+
+	file_close(f_out);
+	free(c_out);
+
+return 1;
+}
+
 
 /*******************************************************************/
